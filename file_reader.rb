4ddd7c4133a90a9b478d66a7ac53b050b0c4affb
ovolkov <lifecoder.ua@gmail.com>
2016-07-19 18:53:50 +0300

    read and merge values

--- file_reader.rb
+++ file_reader.rb
@@ -4,27 +4,52 @@ BATCH_SIZE = 100
 
 class FileReader
   attr_reader :century
-  @csv 
+  @csv
   @buffer
+  @next_line
 
   def initialize path
     # open file
     handle = File.open(path)
-    @csv = CSV.new(handle, col_sep: ':')
+    @csv = CSV.new(handle, col_sep: ':', converters: :numeric)
+    @next_line = nil
     @buffer = []
+    pick
 
-p pick 
     # read+parse first entry
     # set next century
   end
 
+  def eof?
+    @next_line.nil?
+  end
+
   def next
     # return data block until next century, data length limit or EOF 
+    @buffer = []
+    current_century = @century
+
+    BATCH_SIZE.times do
+      readline
+      century_changed = current_century != @century
+      break if century_changed || eof?  
+    end
+
+    @buffer
   end
 
   def pick
     # [date, val]
-    @csv.readline 
+    @next_line = @csv.readline 
+    return nil if @next_line.nil?
+    @century = @next_line.first[0..1]
+
+    @next_line
+  end
+
+  def readline 
+    @buffer << @next_line
+    pick
   end
 end
 
--- merger.rb
+++ merger.rb
@@ -3,18 +3,32 @@ require './file_reader.rb'
 class Merger
   def initialize source_folder = './data/input/', result_file = './data/output.csv'
     @file_readers = []
-    @target_century = nil
 
     # initialize FileReaders
     input_files = Dir.glob( File.join(source_folder, '*') )
     input_files.each do |path|
       @file_readers.push FileReader.new(path)
     end
-    
-    # compute starting point (earliest century)
+  end
+
+  def next_target_century
+    @file_readers.min_by(&:century).century
   end
 
   def run
     # iterate through centuries and compile ouput file
+    until @file_readers.empty? do
+      merge_results = {}
+
+      target_century = self.next_target_century
+      @file_readers.select { |file_reader| file_reader.century == target_century }.each do |file_reader|
+        merge_results.merge!(file_reader.next.to_h) { |k, val1, val2| val1 + val2 }
+        # p file_reader.next.inspect
+      end
+
+      @file_readers.reject!(&:eof?)
+      # #sort => array
+      p merge_results.sort.inspect
+    end
   end
 end
